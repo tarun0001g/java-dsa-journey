d94c07129f40d2aa5b214a3a5f02eb6f6e756fce
tarun0001g <tarun0003g@gmail.com>
2026-03-30 20:34:57 +0530

    Practised Array Basics

---
+++ Arrays/Practice.java
@@ -0,0 +1,65 @@
+
+package Arrays;
+
+import java.util.Scanner;
+
+public class Practice{
+  public static void main(String[] args) {
+
+  //     System.out.println("Tarun");
+
+  //   //Declaration of an array
+  //  // int marks[] = new int[3];
+  //   int marks[] = {99, 97, 90};
+  //   //Assigning Values
+  //   // marks[0]=99;
+  //   // marks[1]=97;
+  //   // marks[2]=90;
+
+  //   //System.out.println(marks);//This will not actually shows the values of an array 
+  //   //System.out.println(marks[2]);
+  //   //Print all Values;
+  //   for(int i=0; i<3; i++){
+  //     System.out.println(marks[i]);
+  //   }
+
+  //---------------------------------------------------------------------------------------------------------
+  //TAKING INPUT IN AN ARRAY
+  // Scanner sc = new Scanner(System.in);
+  // System.out.print("Enter size of an array: ");
+  // int size = sc.nextInt();
+  // int marks[] = new int[size];
+  
+  // System.out.println("Enter values in array: ");
+  // //Input in array
+  // for(int i=0; i<size; i++){
+  //   marks[i] = sc.nextInt();
+  // }
+
+  // //Printitng the array
+  // for(int i=0; i<marks.length; i++){
+  //   System.out.print(marks[i]+ " ");
+  // }
+  //--------------------------------------------------------------------------------------------------------
+  //SEARCHING AN ELEMENT IN ARRAY
+  Scanner sc = new Scanner(System.in);
+  System.out.print("Enter size of an array: ");
+  int size = sc.nextInt();
+  int marks[] = new int[size];
+  System.out.println("Enter values in array: ");
+  //Input in array
+  for(int i=0; i<size; i++){
+    marks[i] = sc.nextInt();
+  }
+
+  System.out.print("Enter the target element : ");
+  int n = sc.nextInt();
+
+  //Searching an element
+  for(int i=0; i<marks.length ; i++){
+    if(n == marks[i]){
+      System.out.println("The element found at index: "+i);
+    }
+  }
+  }
+}
---
+++ Arrays/TwoDArray.java
@@ -0,0 +1,58 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class TwoDArray {
+  public static void main(String[] args) {
+      // Scanner sc =  new Scanner(System.in);
+      // System.out.println("Enter No of Rows and Columns :");
+      // int rows = sc.nextInt();
+      // int cols = sc.nextInt(); 
+      // int numbers[][] = new int[rows][cols];
+
+      // System.out.println("Enter Values of 2D array:- ");
+
+      // //Input of Array
+      // for(int i=0; i<rows; i++){
+      //   for (int j=0; j<cols; j++){
+      //     numbers[i][j] = sc.nextInt();
+      //   }
+      // }
+
+      // //Printing the array
+      // System.out.println("Your 2D Array is :-");
+      // for(int i=0; i<rows; i++){
+      //   for (int j=0; j<cols; j++){
+      //     System.out.print(numbers[i][j] + " ");
+      //   }
+      //   System.out.println();//for next line
+      // }
+//------------------------------------------------------------------------------------------------------------------------------------------
+      //Find an Element from 2D Array
+      Scanner sc =  new Scanner(System.in);
+      System.out.println("Enter No of Rows and Columns :");
+      int rows = sc.nextInt();
+      int cols = sc.nextInt(); 
+      int numbers[][] = new int[rows][cols];
+
+      System.out.println("Enter Values of 2D array:- ");
+
+      //Input of Array
+      for(int i=0; i<rows; i++){
+        for (int j=0; j<cols; j++){
+          numbers[i][j] = sc.nextInt();
+        }
+      }
+
+      System.out.print("Enter the target element to search:");
+      int x = sc.nextInt();
+      for(int i=0; i<rows; i++){
+        for(int j=0; j<cols; j++){
+          if(numbers[i][j]==x){
+            System.out.println("The number found at index: (" + i + ","+j+")");
+          }
+        }
+      }
+
+  }
+}
